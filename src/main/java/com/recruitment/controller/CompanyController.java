e6a59bfe837a51daf1e3a3ec2d3a81f570831db1
jeaslyn sm <code.jessi.sm@gmail.com>
2024-08-01 23:46:50 +0900

    0801

--- src/main/java/com/recruitment/controller/CompanyController.java
+++ src/main/java/com/recruitment/controller/CompanyController.java
@@ -1,7 +1,9 @@
 package com.recruitment.controller;
 
+import com.recruitment.dto.CompanyDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CompanyController {
@@ -9,5 +11,11 @@ public class CompanyController {
     @GetMapping("/register")
     public String createCompany() {
         return "company/register";
+    }
+
+
+    @PostMapping("/registerOk")
+    public CompanyDTO register(CompanyDTO companyDTO) {
+        return companyDTO;
     }
 }
