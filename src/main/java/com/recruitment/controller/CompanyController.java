656dd7e3fae5187adb426b191dc568908b317457
jeaslyn sm <code.jessi.sm@gmail.com>
2024-08-01 23:23:30 +0900

    0801

--- src/main/java/com/recruitment/controller/CompanyController.java
+++ src/main/java/com/recruitment/controller/CompanyController.java
@@ -1,7 +1,13 @@
 package com.recruitment.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CompanyController {
+
+    @GetMapping("/register")
+    public String createCompany() {
+        return "company/register";
+    }
 }
